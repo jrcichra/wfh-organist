d3e6cd3341013c193fcd143f7b21967e74f4923f
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-03-07 22:25:55 -0500

    another 60

--- upnpc.sh
+++ upnpc.sh
@@ -4,4 +4,4 @@ set -x
 IP=$(ip addr | grep eth0 | grep inet | awk '{print $2}' | cut -d"/" -f1)
 WIREGUARD_PORT=51820
 HOURS=3 # your cron should be faster than this
-upnpc -a ${IP} ${WIREGUARD_PORT} ${WIREGUARD_PORT} udp $((HOURS*60))
+upnpc -a ${IP} ${WIREGUARD_PORT} ${WIREGUARD_PORT} udp $((HOURS*60*60))
