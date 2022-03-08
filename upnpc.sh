8f931f55301d763b0862722411db377dd21529d5
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-03-07 23:41:26 -0500

    find default interface

--- upnpc.sh
+++ upnpc.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # Run this from crontab to keep a wireguard port open
 set -x
-IP=$(ip addr | grep eth0 | grep inet | awk '{print $2}' | cut -d"/" -f1)
+INTERFACE=$(route -n  | grep '^0.0.0.0' | head -1 | awk '{print $8}')
+IP=$(ip addr | grep ${INTERFACE} | grep inet | awk '{print $2}' | cut -d"/" -f1)
 WIREGUARD_PORT=51820
 HOURS=3 # your cron should be faster than this
 upnpc -a ${IP} ${WIREGUARD_PORT} ${WIREGUARD_PORT} udp $((HOURS*60*60))
