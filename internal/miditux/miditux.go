822d430226792fb7e3405196df9ba48e5707f1f0
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-03-12 20:34:37 -0500

    add space

--- internal/miditux/miditux.go
+++ internal/miditux/miditux.go
@@ -24,23 +24,24 @@ func midiTuxPrint(clr color.Attribute, t interface{}, ms int64) {
 	color.Set(clr)
 	switch m := t.(type) {
 	case types.NoteOn:
-		log.Printf("Type: %s, Channel: %2d, Key: %3d, Velocity: %2d, %d ms%s\n", "Note On ", m.Channel+1, m.Key, m.Velocity, ms, slowStr)
+		slowStr := ""
+		log.Printf("Type: %s, Channel: %2d, Key: %3d, Velocity: %2d, %d ms %s\n", "Note On ", m.Channel+1, m.Key, m.Velocity, ms, slowStr)
 	case types.NoteOff:
-		log.Printf("Type: %s, Channel: %2d, Key: %3d, Velocity: %2d, %d ms%s\n", "Note Off", m.Channel+1, m.Key, 0, ms, slowStr)
+		log.Printf("Type: %s, Channel: %2d, Key: %3d, Velocity: %2d, %d ms %s\n", "Note Off", m.Channel+1, m.Key, 0, ms, slowStr)
 	case types.ProgramChange:
 		log.Printf("Type: %s, Channel: %2d, Program: %2d, %d ms%s\n", "Program Change", m.Channel+1, m.Program, ms, slowStr)
 	case types.Aftertouch:
 		log.Printf("Type: %s, Channel: %2d, Pressure: %2d, %d ms%s\n", "Aftertouch", m.Channel+1, m.Pressure, ms, slowStr)
 	case types.ControlChange:
-		log.Printf("Type: %s, Channel: %2d, Controller: %2d, Value: %2d, %d ms%s\n", "Control Change", m.Channel+1, m.Controller, m.Value, ms, slowStr)
+		log.Printf("Type: %s, Channel: %2d, Controller: %2d, Value: %2d, %d ms %s\n", "Control Change", m.Channel+1, m.Controller, m.Value, ms, slowStr)
 	case types.NoteOffVelocity:
-		log.Printf("Type: %s, Channel: %2d, Key: %3d, Velocity: %2d, %d ms%s\n", "Note Off Velocity", m.Channel+1, m.Key, m.Velocity, ms, slowStr)
+		log.Printf("Type: %s, Channel: %2d, Key: %3d, Velocity: %2d, %d ms %s\n", "Note Off Velocity", m.Channel+1, m.Key, m.Velocity, ms, slowStr)
 	case types.Pitchbend:
-		log.Printf("Type: %s, Channel: %2d, Value: %3d, AbsValue: %4d, %d ms%s\n", "Pitchbend", m.Channel+1, m.Value, m.AbsValue, ms, slowStr)
+		log.Printf("Type: %s, Channel: %2d, Value: %3d, AbsValue: %4d, %d ms %s\n", "Pitchbend", m.Channel+1, m.Value, m.AbsValue, ms, slowStr)
 	case types.PolyAftertouch:
-		log.Printf("Type: %s, Channel: %2d, Key: %3d, Pressure: %2d, %d ms%s\n", "Poly Aftertouch", m.Channel+1, m.Key, m.Pressure, ms, slowStr)
+		log.Printf("Type: %s, Channel: %2d, Key: %3d, Pressure: %2d, %d ms %s\n", "Poly Aftertouch", m.Channel+1, m.Key, m.Pressure, ms, slowStr)
 	case types.Raw:
-		log.Printf("Type: %s, Content: %x, %d ms%s\n", "Raw", m.Data, ms, slowStr)
+		log.Printf("Type: %s, Content: %x, %d ms %s\n", "Raw", m.Data, ms, slowStr)
 	default:
 		log.Printf("Type: %s,\n", "Unknown")
 	}
