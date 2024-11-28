c51e5f5198a4e9939c5e00a5b1a5a579a1d8c462
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2024-11-27 22:43:39 -0500

    fix up for new react-router

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./Home";
 
 function App() {
